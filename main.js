33cb0f7a57e5f2ab882beca7b258881383bb6a7e
Ishaanispro <121791398+Ishaanispro@users.noreply.github.com>
2023-03-14 19:38:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var firstname = "Ranbir";
+console.log(firstname);
+
+var lastname = "Kapoor";
+console.log(lastname);
+
+var fullname = firstname.concat(lastname)
+console.log(fullname);
+
+var a = 5;
+var b = 4;
+var c = 3;
+var d = a+b+c
+console.log(a,b,c,d) 
+
+var text = "The sum of 5, 4, 3 is 12.";
+console.log(text);
